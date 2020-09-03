9dcd7bd60903ab36e40520c0440b01b050571d79
Keriel <keriel@jupiterflow.com>
2020-09-03 21:35:43 +0900

    :sparkles: 양방향 연관관계 매핑 실습
    실무에선 권장하지 않음. 비즈니스적으로 관리하기 까다롭다.
    
    차라리 각자 조회하고 쿼리를 만드는게 낫다.

--- application/02_jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ application/02_jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -13,14 +15,53 @@ public class Order {
     private Long id;
 
     // 객체 그래프 탐색에 최적화. 객체지향적 설계
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
     private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 
 }
