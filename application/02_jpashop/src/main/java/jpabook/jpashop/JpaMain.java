847009f23db6c2067449a807c73e6db282a663bf
Keriel <keriel@jupiterflow.com>
2020-09-04 02:58:45 +0900

    :sparkles: 상속관계 매핑 실전 예제 실습 수행

--- application/02_jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ application/02_jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,7 +1,6 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.Book;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -18,14 +17,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Order order = new Order();
-            em.persist(order);
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("TEST");
 
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
-
-            em.persist(orderItem);
-
+            em.persist(book);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
